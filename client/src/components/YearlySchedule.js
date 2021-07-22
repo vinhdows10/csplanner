625405a2f2216c605e50210662702cd813c5d11d
vinhdows10 <vinhkhang314@gmail.com>
2021-07-21 19:16:52 -0700

    Added dragging functionality to course selection using React Hooks

--- client/src/components/YearlySchedule.js
+++ client/src/components/YearlySchedule.js
@@ -1,14 +1,93 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import "../css/YearlySchedule.css";
 
 function YearlySchedule(props) {
+    const [list, setList] = useState(props.data);
+    const [dragging, setDragging] = useState(false);
+
+    const dragCourse = useRef();
+    const dragNode = useRef();
+
+    const handleDragStart = (e, params) => {
+        console.log('drag starting...', params);
+        dragCourse.current = params;
+        dragNode.current = e.target;
+        dragNode.current.addEventListener('dragend', handleDragEnd);
+        setTimeout(() => {
+            setDragging(true);
+        }, 0);
+        setDragging(true);
+    }
+
+    const handleDragEnter = (e, targetCourse) => {
+        console.log('Entering drag...', targetCourse);
+        const currentCourse = dragCourse.current;
+
+        if (e.target !== dragNode.current) {
+            setList(oldList => {
+                let newList = JSON.parse(JSON.stringify(oldList));
+                newList[targetCourse.grpIndex].courses.splice(targetCourse.courseIndex, 0, newList[currentCourse.grpIndex].courses.splice(currentCourse.courseIndex, 1)[0])
+                dragCourse.current = targetCourse;
+                return newList;
+            })
+        }
+    }
+
+    const handleDragEnd = () => {
+        console.log('Ending drag...');
+        dragNode.current.removeEventListener('dragend', handleDragEnd);
+        dragCourse.current = null;
+        dragNode.current = null;
+        setDragging(false);
+    }
+
+    const getStyles = (course) => {
+        if (dragCourse.current.courseIndex === course.courseIndex) {
+            return "course-style current";
+        }
+    
+        return "course-style";
+    }
+
     return (
         <div className="wrapper">
             <div className="heading">
                 {props.year} Year
             </div>
             <div className="yearly-schedule-body">
-            
+                <div className='quarter'>
+                    Fall
+                    {list.map((grp, grpIndex) => (
+                        <div key={grp.title}>
+                        {grp.courses.map((course, courseIndex) => (
+                            <div 
+                                className={dragging ? getStyles(grpIndex, courseIndex) : "course-style"} 
+                                draggable 
+                                key={course} 
+                                onDragStart={(e) => {handleDragStart(e, {grpIndex, courseIndex})}}
+                                onDragEnter={dragging ? (e) => {handleDragEnter(e, {grpIndex, courseIndex})} : null} 
+                            >
+                                {course}
+                            </div>
+                        ))}
+                        </div>
+                    ))}
+                </div>
+                
+                <div className='quarter'>
+                    Winter
+                    
+                </div>
+
+                <div className='quarter'>
+                    Spring
+                    
+                </div>
+
+                <div className='quarter'>
+                    Summer
+                    
+                </div>
             </div>
         </div>
     )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,10 @@ import "../css/Home.css";
 import AddCourses from "../components/AddCourses";
 import YearlySchedule from "../components/YearlySchedule";
 
+const data = [
+	{courseTitle: 'group 1', courses: ['Course 1', 'Course 2', 'Course 3', 'Course 4', 'Course 5', 'Course 6']}
+]
+
 function Home() {
 	return (
 		<div className="main">
@@ -11,10 +15,10 @@ function Home() {
 
 			<div className="schedule-wrapper">
 				<div className="schedule">
-					<YearlySchedule year="Freshman" />
-					<YearlySchedule year="Sophomore" />
-					<YearlySchedule year="Junior" />
-					<YearlySchedule year="Senior" />
+					<YearlySchedule year="Freshman" data={data} />
+					<YearlySchedule year="Sophomore" data={data} />
+					<YearlySchedule year="Junior" data={data} />
+					<YearlySchedule year="Senior" data={data} />
 				</div>
 			</div>
 		</div>
